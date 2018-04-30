4e25b33397376dd5c716e62935f4b044a4ff3573
Nathaniel Budikey <35513839+NatsBud@users.noreply.github.com>
2018-04-30 09:51:25 +0800

    Add files via upload

--- create.php
+++ create.php
@@ -1,70 +1,47 @@
 <?php
-
 require 'db.php';
-
 $message = '';
-
 if (isset ($_POST['name']) && isset($_POST['email'])){
-
 	$name = $_POST['name'];
 	$email = $_POST['email'];
-
 	if(empty($name) && empty($email)){
-		$message = "Fill all fields";
-		
+		$message = "Pleae fill up all fields";	
 	}else{
-
 $sql = 'INSERT INTO people(name, email) VALUES(:name, :email)';
 	$statement = $connection->prepare($sql);
-
 	if($statement->execute([':name' => $name, ':email' => $email])){
 		$message = 'data inserted successfully';
-
+		}
 	}
-
-	
-	}	
 }
 ?>
-
-
-
-
 <?php require 'header.php'; ?>
 <?php require 'navigation.php'; ?>
-
 <div class="container">
 	<div class="card mt-5">
 		<div class="card-header">
-			<h2>Create a person</h2>
+			<h3 style="text-align: center">Create a person</h3>
 		</div>
-		
-	    <div class="card-body">
-	    	<?php if(!empty($message)): ?>
-	    		<div class="alert alert-success">
-	    		<?= $message; ?>
-	    		</div>
-	    	<?php endif; ?>	
-		<form method="POST">
-		<div class="form-group">
-			<label for="name">Name</label>
-			<input type="text" name="name" id="name" class="form-control">
+		<div class="card-body">
+			<?php if(!empty($message)): ?>
+			<div class="alert alert-success">
+				<?= $message; ?>
+			</div>
+			<?php endif; ?>
+			<form method="POST">
+				<div class="form-group">
+					<label for="name">Name</label>
+					<input type="text" name="name" id="name" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="email" name="email" id="email" class="form-control">
+				</div>
+				<div class="form-group">	
+					<button type="submit" class="btn btn-warning">Create user</button>
+				</div>
+			</form>
 		</div>
-
-		<div class="form-group">
-			<label for="email">Email</label>
-			<input type="email" name="email" id="email" class="form-control">
-		</div>
-		<div class="form-group">
-			
-			<button type="submit" class="btn btn-warning">Create a person</button>
-
-		</div>
-		</form>
-	    </div>
 	</div>
 </div>
-
-
-
 <?php require 'footer.php'; ?>
